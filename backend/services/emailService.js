14450a7e3533035ed812d7b99dd0b14eac8ac8e2
Harekam Kaur <harekamk@gmail.com>
2026-07-10 18:43:32 +0530

    change made

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -6,14 +6,18 @@ console.log("EMAIL_PASS =", process.env.EMAIL_PASS);
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
-  secure: false, // TLS via STARTTLS
+  secure: false,
+  requireTLS: true,
 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 
-  family: 4, // Force IPv4
+  tls: {
+    family: 4,
+    rejectUnauthorized: false,
+  },
 });
 transporter.verify((error, success) => {
   if (error) {
